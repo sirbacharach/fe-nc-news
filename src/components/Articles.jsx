4089a6ca04445496faf217537a9a653be4d6da95
robert-allen <allen.robert79@gmail.com>
2023-12-11 13:52:22 +0000

    completes 4-view-all-articles

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,17 +1,25 @@
 import { useEffect, useState } from "react";
 import { getAllArticles } from "./api";
+import ArticleCard from "./ArticleCard";
 
 const Articles = () => {
-    const [articles, setArticles] = useState([])
+  const [articles, setArticles] = useState([]);
 
-    useEffect(() => {
-        getAllArticles().then((response)=>{
-            setArticles(response)
-        })
-    },[])
+  useEffect(() => {
+    getAllArticles().then((response) => {
+      setArticles(response);
+    });
+  }, []);
 
+  return (
+    <>
+      <ul className="articles">
+        {articles.map((article) => {
+          return <ArticleCard article={article} key={article.article_id} />;
+        })}
+      </ul>
+    </>
+  );
 };
-
-
 
 export default Articles;
--- src/components/api.js
+++ src/components/api.js
@@ -3,13 +3,9 @@ import axios from "axios";
 const api = axios.create({ baseURL: "https://robs-nc-news.onrender.com" });
 
 const getAllArticles = () => {
-
-    return (
-    api.get('/api/articles').then((response)=>{
-        console.log(response.data.articles)
-    })
-        )
-
+  return api.get("/api/articles").then((response) => {
+    return response.data.articles;
+  });
 };
 
-export {getAllArticles}
+export { getAllArticles };
