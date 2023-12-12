46597579b3a2010327de134ac5894e4ff37dd7ce
robert-allen <allen.robert79@gmail.com>
2023-12-12 15:48:18 +0000

    adds styling

--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getSingleArticle } from "./api";
 import { patchArticle } from "./api";
+import Comments from "./Comments";
 
 const SingleArticle = () => {
   const [article, setArticle] = useState({});
@@ -38,9 +39,10 @@ const SingleArticle = () => {
   };
 
   return (
+    <>
     <div className="single-item">
       <h2>{article.title}</h2>
-      <img src={article.article_img_url} alt={`${article.title}`} />
+      <img id="all-article-imgs" src={article.article_img_url} alt={`${article.title}`} />
       <p>{article.created_at ? article.created_at.slice(0, 10) : <></>}</p>
       <p>{article.author}</p>
       <p>{article.body}</p>
@@ -54,6 +56,8 @@ const SingleArticle = () => {
         Up Vote
       </button>
     </div>
+    <Comments/>
+    </>
   );
 };
 
