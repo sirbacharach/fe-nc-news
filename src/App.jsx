3fefc8307eff89a9d255d69fd6fea9f044826245
robert-allen <allen.robert79@gmail.com>
2024-01-26 10:56:32 +0000

    gets rid of references to topics page

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import SingleArticle from "./components/SingleArticle";
 import Users from "./components/Users";
 import { Routes, Route } from "react-router-dom";
 import { UserProvider } from "./components/UserContext";
-import Topics from "./components/Topics";
 import Error from "./components/Error";
 
 function App() {
@@ -18,7 +17,6 @@ function App() {
             <Route path="/articles/:article_id" element={<SingleArticle />} />
             <Route path="/users" element={<Users />} />
             <Route path="/" element={<Articles />} />
-            <Route path="/topics" element={<Topics />} />
             {/* <Route path="/*" element={<Error message="Route not found!"/>}/> */}
           </Routes>
       </UserProvider>
